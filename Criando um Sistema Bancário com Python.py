038d4e5aa0871c5f740909da68f2a47f137f455c
PauloHemrich <128193003+PauloHemrich@users.noreply.github.com>
2023-08-10 20:17:15 -0300

    Criando um Sistema Bancário com Python.py

--- Criando um Sistema Bancário com Python.py
+++ Criando um Sistema Bancário com Python.py
@@ -1,16 +1,13 @@
-Deposito = 0
+deposito = 0
+saque = 0
+numeroSaque = 3
+saldo = 0
+extrato = ""
 
-Saque = 0
-
-Limite_Saque = 500
-
-numero_Saque = 3
-
-Extrato = Deposito - Saque
-
-
-
-print("""Bem vindo a sua conta bancaria! No que podemos te ajudar:
+print("Bem vindo ao sistema bancario!!!")
+while True:
+    print("""
+----------------------------------------------
 
 [1]Extrato
 
@@ -19,33 +16,40 @@ print("""Bem vindo a sua conta bancaria! No que podemos te ajudar:
 [3]Deposito
 
 [4]Sair
-
+----------------------------------------------
 """)
 
-
-
-while True:
     menu = input("Digite o número: ")
     if menu == "1":
-        print("O Extrato da sua conta é de: R${:.2f}".format(Extrato))
+        print("\n----------------------------------------------")
+        print(extrato)
+        print("Saldo = R${:.2f}\n ".format(saldo))
+
     elif menu == "2":
-        if numero_Saque ==0:
+        if numeroSaque == 0:
             print("Número de Saques atingido, volte outro dia!")
         else:
-            numero_Saque = numero_Saque - 1
-            Saque = float(input("Qual é o valor do Saque?: "))
-            if Saque > Extrato:
-                print("Você não pode sacar este valor pois você não possui.")
+            numeroSaque = numeroSaque - 1
+            saque = float(input("Qual é o valor do Saque?: "))
+            if (saque > saldo) or (saque < 0):
+                print("Você não pode sacar este valor pois é invalido.")
             else:
-                while Saque > Limite_Saque:
-                    Saque = float(input("O limite do saque é de R$500.00, por favor digite um número menor ou igual: "))
-                    Extrato = Extrato - Saque
+                if saque > 500:
+                    print("O limite do saque é de R$500.00, por favor digite um número menor ou igual: ")
+                else:
+                    saldo = saldo - saque
+                    print("Feito o saque de: R${:.2f}".format(saque))
+                    extrato += f"Saque-> R${saque:.2f}\n"
+                    
     elif menu == "3":
-        Deposito = float(input("Qual o valor para Depositar?: "))
-        Extrato = Deposito + Extrato
-    
+        deposito = float(input("Qual o valor para Depositar?: "))
+        if deposito < 0:
+            print("Você não pode depositar este valor pois é invalido.")
+        else:
+            saldo = saldo + deposito
+            print("Feito o deposito de: R${:.2f}".format(saque))
+            extrato += f"Deposito-> R${deposito:.2f}\n"
 
-    
     elif menu == "4":
         break
     
@@ -53,5 +57,4 @@ while True:
         print("Número invalido, por favor digite o número correspondente!")
 
             
-
 
